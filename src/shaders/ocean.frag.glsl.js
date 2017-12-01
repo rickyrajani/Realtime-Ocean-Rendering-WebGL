98970bc704e85d83e7348efdf5b784ca39e023e1
Ricky Rajani <rickys.rajani@gmail.com>
2017-12-01 14:56:30 -0500

    added blinn phong

--- src/shaders/ocean.frag.glsl.js
+++ src/shaders/ocean.frag.glsl.js
@@ -12,9 +12,11 @@ export default function(params) {
   uniform samplerCube skybox;
   uniform mat3 u_normalMatrix;
   uniform mat3 u_invVT;
+  uniform mat4 u_viewMatrix;
 
   void main() {
-    vec3 albedo = vec3(25.0/255.0, 140.0/255.0 , 190.0/255.0);    
+    // vec3 albedo = vec3(25.0/255.0, 140.0/255.0 , 190.0/255.0);    
+    vec3 albedo = vec3(1.0, 1.0, 1.0);
     vec3 normal = v_normal;
     vec3 pos = v_position;
     
@@ -23,22 +25,24 @@ export default function(params) {
     vec3 sunLight = vec3(1, 75, -5);
     vec3 lightDir = normalize(sunLight - pos);
     float NdotL = clamp(dot(normal, lightDir), 0.1, 1.0);
-    fragColor += albedo * NdotL;
+    // fragColor += albedo * NdotL;
+
+    mat4 invViewMatrix = inverse(u_viewMatrix);
+    vec4 cameraWorldPos = invViewMatrix * vec4(0.0, 0.0, 0.0, 1.0);
+    vec3 V = normalize(cameraWorldPos.xyz - v_position);
+    vec3 H = normalize(lightDir + V);
+    float NdotH = max(dot(H, normal), 0.0);
+    float specular = pow(NdotH, 100.0);
+    fragColor += (albedo + vec3(specular)) * NdotL;
 
     const vec3 ambientLight = vec3(0.25);
-    fragColor += albedo * ambientLight;
+    // fragColor += albedo * ambientLight;
     // v_color = vec4(fragColor, 1.0);
     // v_color = vec4(normal, 1.0);
 
-    /*
-    vec3 N = v_normal;
-    vec3 V = - v_viewCoords;
-    vec3 R = 2.0 * dot(V, N) * N - V;
-    R = u_invVT * R; */
+    fragColor += vec3(texture(skybox, v_R));
 
-    v_color = texture(skybox, v_R);
-
-    
+    v_color = vec4(fragColor, 1.0);
   }
   `;
 }
